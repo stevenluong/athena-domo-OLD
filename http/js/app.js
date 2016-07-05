13b70d322c82bce86a7c05602c2b8265cba09ff3
s <ste.luong@gmail.com>
2016-07-05 17:41:31 +0200

    Authentification management

--- http/js/app.js
+++ http/js/app.js
@@ -3,21 +3,38 @@
 /* App Module */
 
 var mainApp = angular.module('mainApp', [
-  'ngRoute',
-  //'mainAnimations',
-  'mainControllers',
-  'mainFilters',
-  'mainServices'
+        'ngRoute',
+        //'mainAnimations',
+        'mainControllers',
+        'mainFilters',
+        'mainServices',
+        'Devise'
 ]);
 
-mainApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/main.html',
-        controller: 'mainCtrl'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
+mainApp.config(['$routeProvider','AuthProvider',
+        function($routeProvider,AuthProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: 'partials/main.html',
+                    controller: 'mainCtrl'
+                }).
+                when('/login', {
+                    templateUrl: 'partials/login.html',
+                    controller: 'mainCtrl'
+                }).
+                when('/register', {
+                    templateUrl: 'partials/register.html',
+                    controller: 'mainCtrl'
+                }).
+
+            otherwise({
+                redirectTo: '/'
+            });
+            AuthProvider.loginPath('http://slapps.fr/athena/ror/users/sign_in.json');
+            AuthProvider.logoutPath('http://slapps.fr/athena/ror/users/sign_out.json');
+            AuthProvider.registerPath('http://slapps.fr/athena/ror/users.json');
+            //AuthProvider.loginMethod('GET');
+            //AuthProvider.resourceName('user');
+        }
+
+]);
--- http/js/controllers.js
+++ http/js/controllers.js
@@ -3,9 +3,38 @@
 /* Controllers */
 
 var mainControllers = angular.module('mainControllers', []);
-mainControllers.controller('mainCtrl', ['$scope','Sources',
-        function($scope, Sources) {
-            //Sources.getSources().success(function(response){
-            //    console.log(response);
-            //});
+mainControllers.controller('mainCtrl', ['$scope','Auth','$window','$location',
+        function($scope, Auth,$window,$location) {
+            Auth.currentUser().then(function(user) {
+                $scope.user = user;
+                if($window.location.hash=="#/login")
+                    $location.path("/")
+            });
+            $scope.signedIn = Auth.isAuthenticated;
+            $scope.logout = function(){
+                Auth.logout();
+                $scope.user = {};
+            };
+            $scope.login = function(){
+                Auth.login($scope.user).then(function(user) {
+                    $scope.user = user;
+                    $location.path("/");
+                });
+            };
+            $scope.register= function(){
+                Auth.register($scope.user).then(function(user) {
+                    console.log($scope.user);
+                    $scope.user = user;
+                    $location.path("/");
+                });
+            };
+
+            $scope.$on('devise:login', function(event, currentUser) {
+                $scope.user = currentUser;
+                console.log("DEVISE:LOGIN");
+            });
+            $scope.$on('devise:new-session', function(event, currentUser) {
+                $scope.user = currentUser;
+                console.log("DEVISE:NEW SESSION");
+            });
         }]);
