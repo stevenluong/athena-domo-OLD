6a2e457a439a23d01b72b9f762ea1a279bff35c0
s <ste.luong@gmail.com>
2016-07-06 14:59:04 +0200

    server prep

---
+++ node/athena1.sh
@@ -0,0 +1,5 @@
+espeak -ven+f5 -k5 -s150 "Hi Steven, welcome back"
+espeak -ven+f5 -k5 -s150 "Steven, OK" &
+epiphany --display=:0 https://calendar.google.com/ &
+epiphany --display=:0 http://youtube.com/ &
+
